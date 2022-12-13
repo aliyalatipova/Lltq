c49a04ea0ec9478cf559528aeecc6cf369b97d42
User1 <aliyalatipova060706@gmail.com>
2022-12-13 19:15:38 +0300

    треш1

--- app/src/main/java/com/example/lltq/MainActivity.java
+++ app/src/main/java/com/example/lltq/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int t = 3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
